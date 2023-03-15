991306cd9f1845ab7f0d26249766b127f2ec65b1
jrothman-sfdc <45987098+jrothman-sfdc@users.noreply.github.com>
2023-03-15 11:41:41 -1000

    Update server.js

--- server.js
+++ server.js
@@ -1,34 +1,15 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
-app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(__dirname + '/public/index.html');
 });
 
-app.post('/save', (req, res) => {
-  console.log('Save endpoint called');
-  res.status(200).send('Save successful');
-});
-
-app.post('/publish', (req, res) => {
-  console.log('Publish endpoint called');
-  res.status(200).send('Publish successful');
-});
-
-app.post('/validate', (req, res) => {
-  console.log('Validate endpoint called');
-  res.status(200).send('Validation successful');
-});
-
-app.post('/execute', (req, res) => {
-  console.log('Execute endpoint called');
-  res.status(200).send('Execute successful');
+app.get('/config', (req, res) => {
+  res.sendFile(__dirname + '/public/config.html');
 });
 
 app.listen(port, () => {
