d08251debe6725c621b16ec447174f278965f22a
jrothman-sfdc <45987098+jrothman-sfdc@users.noreply.github.com>
2023-03-15 10:36:15 -1000

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.post('/save', (req, res) => {
   console.log('Save endpoint called');
